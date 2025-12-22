0d3ed40b1d21abef8544cafe42569c18cc4ff52d
Franco D Rodriguez <franco.d.r1992@gmail.com>
2025-12-22 14:06:06 -0300

    tag changed

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -34,7 +34,7 @@ export class Home {
 
 factions = ['marte', 'tierra', 'pluton', 'saturno', 'neptuno', 'jupiter'];
   rarities = ['common','rare','epic','legendary','unlimited'];
-  tagsList = [ 'Adivinar','Amanecer','Al atacar','Al golpear','Aniquilar','Asalto','Ataque 360','Ataque rápido','Atrapo','Aturdir','Cargar','Caída libre','Comerciable','Creo','Drenar','Enfermedad','Escudo mágico','estructura','Inicio del turno', 'invocas','Ofrenda tenebrosa', 'otorgo','Primera vez','Provocar','Puñalada','Regeneración','Silencio','Sobrecarga','Toque mortal','Último suspiro','Venganza','Vínculo' ];
+  tagsList = [ 'Adivinar','Amanecer','Al atacar','Al golpear','Aniquilar','Asalto','Ataque 360','Ataque rápido','Atrapo','Aturdir','Cargar','Caída libre','Comerciable','Creo','Drenar','Enfermedad','Escudo mágico','estructura','Inicio del turno', 'invocar','Ofrenda tenebrosa', 'otorgo','Primera vez','Provocar','Puñalada','Regeneración','Silencio','Sobrecarga','Toque mortal','Último suspiro','Venganza','Vínculo' ];
 
   cardForm: FormGroup;
   cards: Card[] = [];
